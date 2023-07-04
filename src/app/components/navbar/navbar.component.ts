551d0fce8c2cff570fb9c305a59a887e85279d64
Nrmanperez <nperez@nicarao.agency>
2023-07-04 17:48:35 -0600

    Add new section and board

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { faClose } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html'
+})
+export class NavbarComponent {
+  isOpen = false;
+  faClose = faClose;
+}
