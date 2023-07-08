d7ebda2d870196df220d000fe0321f2ebef9f1c0
Nrmanperez <nperez@nicarao.agency>
2023-07-08 09:56:43 -0600

    add table component

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -4,12 +4,14 @@ import { HttpClient } from '@angular/common/http';
 import { Product } from 'src/app/models/product.model';
 
 @Component({
-  selector: 'app-scroll',
-  templateUrl: './scroll.component.html'
+  selector: 'app-table',
+  templateUrl: './table.component.html'
 })
-export class ScrollComponent {
+export class TableComponent {
 
   products: Product[] = [];
+  columns: string[] = ['id', 'title', 'price', 'cover'];
+  total: number = 0;
 
   constructor(
     private http: HttpClient
@@ -18,8 +20,9 @@ export class ScrollComponent {
   ngOnInit(): void {
     this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
     .subscribe(data => {
-      this.products = data
+      this.products = data;
+      this.total = this.products.map(item => item.price)
+      .reduce((price, total) => price + total, 0);
     })
   }
-
 }
